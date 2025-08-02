8dc4808b5a8c0859ab667ac51b6e4f25a67f7622
Richy <richy307@gmail.com>
2025-08-02 17:30:35 +0800

    hello world print

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// using namespace std;
+
+
+
+// Test Hello World 
+void testHelloWorld() 
+{
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main() 
+{
+
+    testHelloWorld();
+
+
+    return 0;
+}
